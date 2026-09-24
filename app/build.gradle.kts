1c76f1da1696117af47822a55c9e6e65bcd6898b
ganatalks17-dotcom <ganatalks17@gmail.com>
2026-09-25 02:58:28 +0530

    Add Kotlin KAPT plugin
    Updated plugin versions for Android and Kotlin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
 }
-
 android {
     namespace = "com.personal.loanofficer"
     compileSdk = 35
