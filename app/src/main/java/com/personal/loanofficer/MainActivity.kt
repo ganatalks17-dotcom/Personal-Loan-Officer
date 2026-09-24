e09df6421c402f974d3254ecb2d8025ae895477d
ganatalks17-dotcom <ganatalks17@gmail.com>
2026-09-25 02:08:42 +0530

    Update MainActivity.kt

--- app/src/main/java/com/personal/loanofficer/MainActivity.kt
+++ app/src/main/java/com/personal/loanofficer/MainActivity.kt
@@ -3,16 +3,8 @@ package com.personal.loanofficer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import com.personal.loanofficer.ui.LoginScreen
 import com.personal.loanofficer.ui.theme.PersonalLoanOfficerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,36 +13,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+
             PersonalLoanOfficerTheme {
-                PersonalLoanOfficerApp()
-            }
-        }
-    }
-}
 
-@androidx.compose.runtime.Composable
-fun PersonalLoanOfficerApp() {
+                LoginScreen(
+                    onLoginSuccess = { name, mobile ->
 
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
+                        // Home screen will be connected here later.
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-
-            Text(
-                text = "PERSONAL LOAN OFFICER"
-            )
-
-            Button(
-                onClick = { }
-            ) {
-                Text("NORMAL EMI CALCULATOR")
+                    }
+                )
             }
         }
     }
