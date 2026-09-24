2b42ddebd778245347d3115e3650e7aa729f95a0
ganatalks17-dotcom <ganatalks17@gmail.com>
2026-09-25 02:03:36 +0530

    Create app/src/main/java/com/personal/loanofficer/ui/theme/Color.kt

---
+++ app/src/main/java/com/personal/loanofficer/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.personal.loanofficer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LoanBlue = Color(0xFF1565C0)
+val LoanDarkBlue = Color(0xFF0D47A1)
+val LoanLightBlue = Color(0xFFE3F2FD)
+
+val LoanWhite = Color(0xFFFFFFFF)
+val LoanBlack = Color(0xFF000000)
+
+val LoanGray = Color(0xFF757575)
+val LoanLightGray = Color(0xFFF5F5F5)
+
+val LoanGreen = Color(0xFF2E7D32)
+val LoanRed = Color(0xFFC62828)
